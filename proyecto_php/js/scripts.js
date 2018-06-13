15797cefc64c923b355f9d65816cae84a4368482
Ivan Hernandez Fuentes <32276409+iherfue@users.noreply.github.com>
2018-06-13 10:49:58 +0100

    Update scripts.js

--- proyecto_php/js/scripts.js
+++ proyecto_php/js/scripts.js
@@ -86,16 +86,16 @@ $(document).ready(function(){
 function hora(hora, minuto, segundo,dia){
 		segundo = segundo + 1;
 		if(segundo == 60) {
-            minuto = minuto + 1;
-            segundo = 0;
-        if(minuto == 60) {
-            minuto = 0;
-            hora = hora + 1;
-        if(hora == 24) {
-            hora = 0;
-            }
-          }
-		}
+           	 minuto = minuto + 1;
+           	 segundo = 0;
+       	        if(minuto == 60) {
+                 minuto = 0;
+                 hora = hora + 1;
+                if(hora == 24) {
+                   hora = 0;
+               	 }
+               }
+	     }
 		if(hora < 10) hora = '0' + hora;
 		if(minuto < 10) minuto = '0' + minuto;
 		if(segundo < 10) segundo = '0' + segundo;
